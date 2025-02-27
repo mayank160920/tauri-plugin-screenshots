d9f18f0f14c136e6efc83cc21a30cfec45c0ab1b
ayang <473033518@qq.com>
2025-02-27 22:36:03 +0800

    chore: updated release configuration

--- .release-it.ts
+++ .release-it.ts
@@ -2,7 +2,7 @@ import type { Config } from "release-it";
 
 export default {
   git: {
-    commitMessage: "tauri-plugin-fs-pro-api v${version}",
+    commitMessage: "tauri-plugin-screenshots-api v${version}",
     tagName: "v${version}",
   },
   npm: {
